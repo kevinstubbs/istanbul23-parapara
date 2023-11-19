6eb8701079fddfb9a95568b69014542aa4e405fa
Kevin Stubbs <kevin@pubgenius.io>
2023-11-19 07:18:02 +0300

    Format HRMI data.

--- website/components/CountryInfo.tsx
+++ website/components/CountryInfo.tsx
@@ -6,6 +6,8 @@ interface Props {
 }
 
 export const CountryInfo = ({ country }: Props) => {
+  const matchedHRMI = hrmiData[iso3311a2.getCountry(country)];
+
   return (
     <div>
       <img
@@ -14,9 +16,35 @@ export const CountryInfo = ({ country }: Props) => {
         src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${country}.svg`}
       />
       <div>HRMI Data</div>
-      <div>
-        {JSON.stringify(hrmiData[iso3311a2.getCountry(country)], null, 4)}
-      </div>
+      {matchedHRMI ? (
+        <div>
+          <div>
+            Overall quality of life score:{" "}
+            {matchedHRMI.LMY_QofL_All_GBScore || "unknown"}
+          </div>
+          <div>
+            Overall education score:{" "}
+            {matchedHRMI.LMY_Education_All_GBScore || "unknown"}
+          </div>
+          <div>
+            Overall food score: {matchedHRMI.LMY_Food_All_GBScore || "unknown"}
+          </div>
+          <div>
+            Overall health score:{" "}
+            {matchedHRMI.LMY_Health_All_GBScore || "unknown"}
+          </div>
+          <div>
+            Overall housing score:{" "}
+            {matchedHRMI.LMY_Housing_All_GBScore || "unknown"}
+          </div>
+          <div>
+            Overall work score: {matchedHRMI.LMY_Work_All_GBScore || "unknown"}
+          </div>
+          <div>Last updated in {matchedHRMI.Year}</div>
+        </div>
+      ) : (
+        <div>Unable to find matching HRMI data on this country</div>
+      )}
     </div>
   );
 };
